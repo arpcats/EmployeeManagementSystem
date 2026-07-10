48d9fd67ca68c61225e1118908759094a2b392aa
Tony <anthony.payumo@century-phirst.com>
2026-07-11 06:02:08 +0800

    feat(infrastructure): add data access configuration

---
+++ backend/EmployeeManagement.Infrastructure/Persistence/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement.Infrastructure.Persistence.Configurations
+{
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.ToTable("Employees");
+
+            builder.HasKey(e => e.EmployeeId);
+
+            builder.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.LastName).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.Email).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Department).IsRequired().HasMaxLength(50);
+            builder.Property(e => e.EmploymentStatus).IsRequired().HasConversion<string>().HasMaxLength(20);
+
+            builder.HasIndex(e => e.Email).IsUnique();
+
+        }
+    }
+}
